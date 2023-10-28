d0d37d390be04449735f3be1ad5f857121729162
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-10-28 21:28:58 +0700

    add and update global variables

--- Lab3/Core/Inc/global.h
+++ Lab3/Core/Inc/global.h
@@ -16,20 +16,26 @@
 #define AUTO_GREEN 		3
 #define AUTO_YELLOW 	4
 
-#define MANUAL_RED 			18
-#define MANUAL_GREEN 		19
-#define MANUAL_YELLOW 	20
+#define NORMAL_MODE		1
+#define MANUAL_RED 			2
+#define MANUAL_YELLOW 	3
+#define MANUAL_GREEN 		4
 
 #define NO_7_SEGMENT_LIGHTS		4
+#define MAX_TIME_DURATION			99		// The value of time duration is in a range of 1 - 99.
 
-extern int blink;					// blinking status
-extern int status_light_1; 	// status for light 1
-extern int status_light_2; 	// status for light 2
+extern int blink;						// blinking status
+extern int status;					// status for traffic mode
+extern int status_light_1; 		// status for light 1
+extern int status_light_2; 		// status for light 2
 extern int counter_light_1; 	// counter for light 1
 extern int counter_light_2; 	// counter for light 2
 extern int counter_lights;
+extern int counter_temp; 		// temporary counter
 extern int RED_LED_Delay;
 extern int GREEN_LED_Delay;
 extern int YELLOW_LED_Delay;
+
+void reset_to_normal_mode();
 
 #endif /* INC_GLOBAL_H_ */
--- Lab3/Core/Src/global.c
+++ Lab3/Core/Src/global.c
@@ -8,12 +8,22 @@
 #include "global.h"
 
 int blink = 0;
+int status = NORMAL_MODE;
 int status_light_1 = INIT;
 int status_light_2 = INIT;
 int counter_light_1 = 0;
 int counter_light_2 =0;
-int counter_lights = 0;
+int counter_lights = 1;
+int counter_temp = 0;
 int RED_LED_Delay = 5000;
 int GREEN_LED_Delay = 3000;
 int YELLOW_LED_Delay = 2000;
+
+void reset_to_normal_mode() {
+	status = NORMAL_MODE;
+	status_light_1 = INIT;
+	status_light_2 = INIT;
+	blink = 0;
+	counter_temp = 0;
+}
 
